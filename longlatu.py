ad0f4353a1e935753c0ff1f576d6cb0f080c5ea4
vagrant <vagrant@vagrant.vm>
2017-02-24 07:20:20 +0000

    Google map and markers are added

--- longlatu.py
+++ longlatu.py
@@ -8,27 +8,31 @@ def getlatlong():
 	temples = Temple.query.all()
 
 	for temple in temples:
+		
+		if temple.zipcode = "OR" 
 		address = temple.address + " " + temple.city + " " + temple.state + " " + temple.zipcode 
 		print address
 		#address = "683 Sutter Street, San Francisco, CA"
 
 
-
 		r = requests.get("https://maps.googleapis.com/maps/api/geocode/json?address="+address+"&key=AIzaSyDXi3yJfXgSN2oApiDtrVa7Nc8kiWwz4DQ")
 		adict = r.json()
+		print adict
 
 		lat = adict["results"][0]["geometry"]["location"]["lat"]
 		lon = adict["results"][0]["geometry"]["location"]["lng"]
+
 
 		temple.latitude = lat
 		temple.longitude = lon
 
 		db.session.commit()
 
-		#print lat, lon
-
-
+		print lat, lon
 
 if __name__ == "__main__":
     connect_to_db(app)
     db.create_all()
+
+47.686353
+-122.133692
